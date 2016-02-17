34eb961756cffdcad2d0f44bd6ffdf1036bff954
davidgiordano12 <davidgiordano12@gmail.com>
2016-02-16 19:46:01 -0700

    Create Cardinal.java

---
+++ Cardinal.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+public class Cardinal extends AbstractBird {
+
+private boolean up;
+
+public Cardinal (int x, int y) {
+  super(x, y, Color.RED);
+  up = true;
+}
+
+public void fly(){
+  if (up == true){
+    getPosition().y--;
+  } else {
+    getPosition().y++;
+  }
+
+  if (getPosition().getY() == 0) {
+    up = false;
+  } else if (getPosition().getY() == 19)
+    up = true;
+ }
+}
