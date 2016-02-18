440b87111b2b747a1d5e4710942562e30343b6b7
jjgreer <jj.greer@pack.csupueblo.edu>
2016-02-18 11:40:59 -0700

    Create AbstractBird.java

---
+++ AbstractBird.java
@@ -0,0 +1,34 @@
+/*
+ * AbstractBird.java"
+ * Joshua Greer
+ * February 16, 2016
+ * Java 271 HW2
+ */
+import java.awt.*;
+public abstract class AbstractBird implements Bird {
+  private int x;
+  private int y;
+  private Color color;
+  private Point position;
+
+AbstractBird(int x, int y, Color color) {
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.position = new Point(x,y);
+
+ }
+
+  public Color getColor(){
+  return color;
+  }
+  public Point getPosition(){
+    return position;
+  }
+  public int getX(){return x;}
+  public int getY(){return y;}
+
+  public abstract void fly();
+
+  }
+
