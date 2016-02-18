b106394ee75a8fc2b67b8cebaed314faa88bf0a0
Matt Ham <mhsnipeorama787@gmail.com>
2016-02-18 11:19:52 -0700

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,13 @@
+/* Project: Aviary.java
+* Name: Matt S. Ham
+* Date: Feb 8, 2016
+* Desc: 
+*/
+
+import java.awt.*;
+
+interface Bird{
+  public Color getColor();
+  public Point getPosition();
+  public void fly();
+}
