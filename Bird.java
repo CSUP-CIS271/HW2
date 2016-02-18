c81d586a254bd2224169c1cccfd6cbb76344e9f1
John Voss <johnny.voss@gmail.com>
2016-02-17 21:11:25 -0700

    Update Bird.java
    Added comments

--- Bird.java
+++ Bird.java
@@ -2,8 +2,8 @@ import java.awt.*;
 public interface Bird {
   public Point getPosition();
   public Color getColor();
-  public int getX();
-  public int getY();
+  public int getX();//not necessary in the interface, variables declared in the abstract class
+  public int getY();//not necessary in the interface, variables declared in the abstract class
   public void fly();
 
 }
