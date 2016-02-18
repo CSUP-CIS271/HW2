9c025071b5243ad8d42e5676c7c2a05059b7b466
jjgreer <jj.greer@pack.csupueblo.edu>
2016-02-18 11:41:31 -0700

    Create Bird.java

---
+++ Bird.java
@@ -0,0 +1,14 @@
+/*
+ * Bird.java"
+ * Joshua Greer
+ * February 16, 2016
+ * Java 271 HW2
+ */
+import java.awt.*;
+public interface Bird {
+ public Point getPosition();
+ public Color getColor();
+ public int getX();
+ public int getY();
+ public void fly();
+}
